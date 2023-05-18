2cfe2e72999e1c21b5c791ee0ed5a78cce6a93ed
Shamiur Rahman Ramim <85860293+qpeano@users.noreply.github.com>
2023-05-18 12:42:08 +0200

    Update ExerciseLoggerWindow.java

--- WorkoutLogger/ExerciseLoggerWindow.java
+++ WorkoutLogger/ExerciseLoggerWindow.java
@@ -24,11 +24,86 @@ public class ExerciseLoggerWindow extends JFrame implements ActionListener {
 	// internal
 
 	private ExerciseLogger logger; // logs the exercise info for later use
-    private LocalDate dateOfToday; // date of the day
-    private DateTimeFormatter dateFormatter; // formatts the date
-    private final String correctFormat = "yyyy-MM-dd"; // the format
+    // private LocalDate dateOfToday; // date of the day
+    // private DateTimeFormatter dateFormatter; // formatts the date
+    // private final String correctFormat = "yyyy-MM-dd"; // the format
 
     /* METHODS - constructor */
 
-    
+    /**
+     * Constructor
+     *
+     * @param fileName the name of the file that houses all the workouts
+     */
+    public ExerciseLoggerWindow(String fileName) {
+
+        super("Exercise Logger");
+        this.setSize(500, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setUpApp(fileName);
+    }
+
+    /* METHODS - internal */
+
+    /**
+     * Methods initializes objects and puts them in view
+     *
+     */
+    public void setUpApp(String fileName) {
+
+        this.makeComponents();
+        this.addComponentsToView();
+
+        try {
+
+            this.logger = new ExerciseLogger(fileName);
+        }
+        catch (IOException exceoption) {
+
+            JOptionPane.showMessageDialog(ExerciseLoggerWindow.this,
+            "A PROBLEM HAS OCCURED WITH DATACOLLECTION:\n" + exception.getMessage() + "\nPLEASE CHECK FILE OR TRY RESTARTING THE APP",
+            "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    /**
+     * Method initializes all starting objects
+     *
+     */
+    private void makeComponents() {
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String date = currentDate.format(formatter);
+
+		this.dateInputField = new JTextField(date);
+		this.searchWorkoutButton = new JButton("search");
+		this.makeNewWorkoutButton = new JButton("new");
+
+		String[] starterText = {"exercise (back-squat)", "sets (2)", "reps (8)", "intensity (79kg)"};
+		this.exerciseInfoInputFields = new JTextField[starterText.length];
+
+		for (int i = 0; i < starterText.length; i++) {
+
+			this.exerciseInfoInputFields[i] = new JTextField(starterText[i]);
+			this.exerciseInfoInputFields[i].setVisible(false);
+		}
+
+		this.saveExerciseButton = new JButton("save");
+		this.saveExerciseButton.setVisible(false);
+
+		this.exercises = new ArrayList<>();
+		this.compareWorkoutsButton = new JButton("compare");
+		this.compareWorkoutsButton.setVisible(false);
+    }
+
+    /**
+     * Method adds all created components to view
+     *
+     */
+    public void addComponentsToView() {
+
+        
+    }
 }
